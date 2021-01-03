9ff9c8d7a65135fec9f3e9954f14729e8cb394ba
Eslam Magdy <eslam3400@gmail.com>
2021-01-03 12:53:08 +0200

    tested <3

---
+++ submit.php
@@ -0,0 +1,17 @@
+<?php 
+    $uploadDir = 'uploads/'; 
+    if(isset($_POST['name']) || isset($_POST['email']) || isset($_POST['file'])){ 
+        $uploadedFile = ''; 
+        if(!empty($_FILES["file"]["name"])){ 
+            $fileName = basename($_FILES["file"]["name"]); 
+            $targetFilePath = $uploadDir . $fileName; 
+            $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
+            $allowTypes = array('jpg', 'png', 'jpeg'); 
+            if(in_array($fileType, $allowTypes)){ 
+                if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){ 
+                    $uploadedFile = $fileName; 
+                }
+            }
+        }
+    }
+?>
